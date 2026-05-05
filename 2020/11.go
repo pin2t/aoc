9a2f8ca2ec1dcb965bbe15861f4329645303ee27
pin2t <pin2t@mail.ru>
2026-05-05 09:03:40 +0500

    day 11

--- 2020/11.go
+++ 2020/11.go
@@ -7,22 +7,24 @@ import "fmt"
 func main() {
     type pos struct { r, c int }
     var scanner = bufio.NewScanner(os.Stdin)
-    var layout = make(map[pos]byte)
-    var r = 0
+    var initial = make(map[pos]byte)
+    var rows, cols, occ, changed = 0, 0, 0, 1
     for scanner.Scan() {
         for c, st := range scanner.Text() {
             if st != '.' {
-                layout[pos{r, c}] = byte(st)
+                initial[pos{rows, c}] = byte(st)
             }
         }
-        r++
+        rows++
+        cols = max(cols, len(scanner.Text()))
     }
-    for {
-        var changed = 0
+    var layout = initial
+    for changed > 0 {
+        changed = 0
         var next = make(map[pos]byte)
         for p, st := range layout {
             var adj = 0
-            for _, d := range []pos{ { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }} {
+            for _, d := range []pos{ { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } } {
                 if layout[pos{ p.r + d.r, p.c + d.c }] == '#' { adj++ }
             }
             if st == 'L' && adj == 0 {
@@ -36,11 +38,41 @@ func main() {
             }
         }
         layout = next
-        if changed == 0 { break }
     }
-    var occ = 0
     for _, st := range layout {
         if st == '#' { occ++ }
     }
-    fmt.Println(occ)
+    fmt.Print(occ)
+    layout = initial
+    changed = 1
+    for changed > 0 {
+        changed = 0
+        var next = make(map[pos]byte)
+        for p, st := range layout {
+            var adj = 0
+            for _, d := range []pos{ { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } } {
+                var view = pos{ p.r + d.r, p.c + d.c }
+                for view.r >= 0 && view.c >= 0 && view.r < rows && view.c < cols {
+                    if layout[view] == '#' { adj++; break }
+                    if layout[view] == 'L' { break }
+                    view = pos{ view.r + d.r, view.c + d.c }
+                }
+            }
+            if st == 'L' && adj == 0 {
+                next[p] = '#'
+                changed++
+            } else if st == '#' && adj >= 5 {
+                next[p] = 'L'
+                changed++
+            } else {
+                next[p] = st
+            }
+        }
+        layout = next
+    }
+    occ = 0
+    for _, st := range layout {
+        if st == '#' { occ++ }
+    }
+    fmt.Println("", occ)
 }
