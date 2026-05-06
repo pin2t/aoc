b03fc9c67b384311a321ee72010fe58c8312bfd1
pin2t <pin2t@mail.ru>
2026-05-06 08:46:10 +0500

    day 13

--- 2020/13.go
+++ 2020/13.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "regexp"
 import "strconv"
+import "strings"
 
 func main() {
     var ts, ids = 0, ""
@@ -10,12 +11,36 @@ func main() {
     fmt.Scanln(&ids)
     var reNum = regexp.MustCompile("\\d+")
     var m, mid = 1000000, 0
-    for _, id := range reNum.FindAllString(ids, -1) {
-        var nid, _ = strconv.Atoi(id)
-        if nid - ts % nid < m {
-            m = nid - ts % nid
-            mid = nid
+    for _, sid := range reNum.FindAllString(ids, -1) {
+        var id, _ = strconv.Atoi(sid)
+        if id - ts % id < m {
+            m = id - ts % id
+            mid = id
         }
     }
-    fmt.Println(m * mid)
+    fmt.Print(m * mid)
+    var start, mul = int64(0), int64(0)
+    for i, sid := range strings.Split(ids, ",") {
+        if sid == "x" { continue }
+        var id, _ = strconv.ParseInt(sid, 10, 64)
+        if start == 0 {
+            start = id
+            mul = id
+            continue
+        }
+        var b = id
+        var goal = start + int64(i)
+        for b != goal && goal % id > 0 {
+            b = (goal / id + 1) * id
+            for b > goal { goal += mul }
+        }
+        var goal2 = goal + mul
+        for b != goal2 && goal2 % id > 0{
+            b = (goal2 / id + 1) * id
+            for b > goal2 { goal2 += mul}
+        }
+        start = goal - int64(i)
+        mul = goal2 - goal
+    }
+    fmt.Println("", start)
 }
