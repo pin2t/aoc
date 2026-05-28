478775e73b7bb7709db4cbed289485a94cc485db
pin2t <pin2t@mail.ru>
2026-05-28 23:25:39 +0500

    day 13 part 2

--- 2019/13.go
+++ 2019/13.go
@@ -4,6 +4,58 @@ import "fmt"
 import "strings"
 import "strconv"
 
+func exec(mem map[int]int64, in chan int64) (out chan int64, halt chan bool) {
+    var sizes = map[int]int{1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 9: 2, 99: 1}
+    out = make(chan int64)
+    halt = make(chan bool)
+    go func() {
+        var m = make(map[int]int64)
+        for i, v := range mem { m[i] = v }
+        var base, i = 0, 0
+        var param = func(n int) int64 {
+            var mode = m[i] / 100
+            if n > 1 { mode /= 10 }
+            if n > 2 { mode /= 10 }
+            switch mode % 10 {
+            case 0: return m[int(m[i+n])]
+            case 1: return m[i+n]
+            case 2: return m[base+int(m[i+n])]
+            }
+            return 0
+        }
+        var write = func(n int, val int64) {
+            var mode = m[i] / 100
+            if n > 1 { mode /= 10 }
+            if n > 2 { mode /= 10 }
+            switch mode % 10 {
+            case 0: m[int(m[i+n])] = val
+            case 1: m[i+n] = val
+            case 2: m[base+int(m[i+n])] = val
+            }
+        }
+        for {
+            var op = m[i] % 100
+            switch op {
+            case 1: write(3, param(1)+param(2))
+            case 2: write(3, param(1)*param(2))
+            case 3: write(1, <-in)
+            case 4: out <- param(1)
+            case 5: if param(1) != 0 { i = int(param(2)) - sizes[int(op)] }
+            case 6: if param(1) == 0 { i = int(param(2)) - sizes[int(op)] }
+            case 7: if param(1) < param(2) { write(3, 1) } else { write(3, 0) }
+            case 8: if param(1) == param(2) { write(3, 1) } else { write(3, 0) }
+            case 9: base += int(param(1))
+            case 99:
+                halt <- true
+                close(out)
+                return
+            }
+            i += sizes[int(op)]
+        }
+    }()
+    return
+}
+
 func main() {
     var s string
     var prog = make(map[int]int64)
@@ -12,60 +64,9 @@ func main() {
         var n, _ = strconv.ParseInt(s, 10, 64)
         prog[i] = n
     }
-    var run = func(input chan int64) (out chan int64, halt chan bool) {
-        var sizes = map[int]int{1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 9: 2, 99: 1}
-        out = make(chan int64)
-        halt = make(chan bool)
-        go func() {
-            var p = make(map[int]int64)
-            for i, v := range prog { p[i] = v }
-            var base, i = 0, 0
-            var param = func(n int) int64 {
-                var mode = p[i] / 100
-                if n > 1 { mode /= 10 }
-                if n > 2 { mode /= 10 }
-                switch mode % 10 {
-                case 0: return p[int(p[i+n])]
-                case 1: return p[i+n]
-                case 2: return p[base+int(p[i+n])]
-                }
-                return 0
-            }
-            var write = func(n int, val int64) {
-                var mode = p[i] / 100
-                if n > 1 { mode /= 10 }
-                if n > 2 { mode /= 10 }
-                switch mode % 10 {
-                case 0: p[int(p[i+n])] = val
-                case 1: p[i+n] = val
-                case 2: p[base+int(p[i+n])] = val
-                }
-            }
-            for {
-                var op = p[i] % 100
-                switch op {
-                case 1: write(3, param(1)+param(2))
-                case 2: write(3, param(1)*param(2))
-                case 3: write(1, <-input)
-                case 4: out <- param(1)
-                case 5: if param(1) != 0 { i = int(param(2)) - sizes[int(op)] }
-                case 6: if param(1) == 0 { i = int(param(2)) - sizes[int(op)] }
-                case 7: if param(1) < param(2) { write(3, 1) } else { write(3, 0) }
-                case 8: if param(1) == param(2) { write(3, 1) } else { write(3, 0) }
-                case 9: base += int(param(1))
-                case 99:
-                    halt <- true
-                    close(out)
-                    return
-                }
-                i += sizes[int(op)]
-            }
-        }()
-        return
-    }
     var in, blocks = make(chan int64), 0
-    var out, halt = run(in)
-    o:
+    var out, halt = exec(prog, in)
+o:
     for {
         select {
         case <-halt: break o
@@ -74,5 +75,34 @@ func main() {
             if <-out == 2 { blocks++ }
         }
     }
-    fmt.Println(blocks)
+    fmt.Print(blocks)
+    prog[0] = 2
+    var score, px = 0, 0
+    in = make(chan int64)
+    out, halt = exec(prog, in)
+ o2:
+    for {
+        select {
+        case <-halt: break o2
+        case x := <-out:
+            <-out
+            var t = <-out
+            if x < 0 {
+                score = int(t)
+            } else {
+                switch t {
+                case 3: px = int(x)
+                case 4:
+                    if int(x) > px {
+                        go func() { in <- 1 }()
+                    } else if int(x) < px {
+                        go func() { in <- -1 }()
+                    } else {
+                        go func() { in <- 0 }()
+                    }
+                }
+            }
+        }
+    }
+    fmt.Println("", score)
 }
